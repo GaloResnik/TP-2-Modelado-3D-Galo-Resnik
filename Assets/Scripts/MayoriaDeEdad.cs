1ac9d8bfe4561cca6b9fb5aa7ce3ea7d572894a8
GaloResnik <galoresnik05@gmail.com>
2022-04-23 17:23:53 -0300

    Initial commit

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadusuario;
+    public Text mitexto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edadusuario >= 18)
+        {
+            mitexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            mitexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+}
---
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniacorrecta;
+    string contraseniausuario;
+    public Text ingresousuario;
+    public GameObject cartelitomsj;
+    public Text textomsj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniacorrecta = "12345";
+        cartelitomsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniausuario = ingresousuario.text;
+        if (contraseniausuario == contraseniacorrecta)
+        {
+            cartelitomsj.SetActive(true);
+            textomsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitomsj.SetActive(true);
+            textomsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
